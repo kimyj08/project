246d9195f0a7d3afa47a9711af66dc7bdd09d534
kimyeju <kimyj9508@gmail.com>
2023-11-08 23:36:21 +0900

    [feat] 커뮤니티 게시판 댓글 변경된 테이블 sql 추가

--- src/main/webapp/sql/reply.sql
+++ src/main/webapp/sql/reply.sql
@@ -8,3 +8,42 @@ CREATE TABLE reply (
 );
 
 select * from REPLY order by r_no desc;
+
+/* 이 아래줄 부분으로 작업 부탁드립니다. */
+
+/* 댓글 */
+CREATE TABLE reply (
+	r_no NUMBER(3) NOT NULL, /* 댓글번호 */
+	cm_bdno NUMBER(3), /* 게시물번호 */
+	r_content VARCHAR2(500), /* 댓글본문 */
+	r_del VARCHAR2(10) DEFAULT '0', /* 삭제여부 */
+	r_date DATE DEFAULT sysdate, /* 등록일시 */
+	r_writer VARCHAR2(20) /* 작성자 */
+);
+
+COMMENT ON TABLE reply IS '댓글';
+
+COMMENT ON COLUMN reply.r_no IS '댓글번호';
+
+COMMENT ON COLUMN reply.cm_bdno IS '게시물번호';
+
+COMMENT ON COLUMN reply.r_content IS '댓글본문';
+
+COMMENT ON COLUMN reply.r_del IS '삭제여부';
+
+COMMENT ON COLUMN reply.r_date IS '등록일시';
+
+COMMENT ON COLUMN reply.r_writer IS '작성자';
+
+CREATE UNIQUE INDEX PK_reply
+	ON reply (
+		r_no ASC
+	);
+
+ALTER TABLE reply
+	ADD
+		CONSTRAINT PK_reply
+		PRIMARY KEY (
+			r_no
+		)
+;
